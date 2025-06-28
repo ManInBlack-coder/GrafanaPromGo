554c8853e9b338a4aa537450e4242cf2bd97c284
Aleksander P <77441538+ManInBlack-coder@users.noreply.github.com>
2025-06-28 15:54:26 +0300

    added prometheus middleware for getting metrics into Go server

--- backend/server.go
+++ backend/server.go
@@ -1,11 +1,45 @@
 package main
 
 import (
- "encoding/json"
- "log"
- "net/http"
- "github.com/gorilla/mux"
+    "encoding/json"
+    "log"
+    "net/http"
+    "time"
+
+    "github.com/gorilla/mux"
+    "github.com/prometheus/client_golang/prometheus"
+    "github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+// defining prometheus metrics 
+
+var (
+	httpRequestsTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total number of HTTP requests",
+		},
+		[]string{"method", "route", "code"},
+	)
+
+	// requests histogram to track request duration
+	httpRequestDuration = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name: "http_request_duration_seconds",
+			Help: "Duration of HTTP requests in seconds",
+			Buckets: prometheus.DefBuckets, // default buckets  
+		},
+		[]string{"method", "route"},
+	)
+)
+
+func init() {
+	// Register the metrics to Prometheus registry
+	prometheus.MustRegister(httpRequestsTotal)
+	prometheus.MustRegister(httpRequestDuration)
+}
+
+
 
 type User struct {
 	ID string `json:"id"`
@@ -21,12 +55,39 @@ func main() {
  // Initialize a new router
  router := mux.NewRouter()
 
- // Define a simple route
- router.HandleFunc("/users", getUsers).Methods("GET")
- router.HandleFunc("/users", createUser).Methods("POST")
+ // add Prometheus metric endpoint 
+ router.Handle("/metrics", promhttp.Handler())
+
+ // added metric collect middleware
+ router.HandleFunc("/users", trackMetrics(getUsers, "/users", "GET")).Methods("GET")
+ router.HandleFunc("/users", trackMetrics(createUser, "/users", "GET")).Methods("POST")
 
 
  log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func trackMetrics(handler http.HandlerFunc, path, method string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now() // reqeust start time
+		lw := &loggingResponseWriter{ResponseWriter: w} // custom response writer to capture status code
+
+		handler.ServeHTTP(lw,r) // start handling the request
+		duration := time.Since(start).Seconds() // calculate duration
+		httpRequestDuration.WithLabelValues(path, method,).Observe(duration)
+
+		httpRequestsTotal.WithLabelValues(method, path, http.StatusText(lw.statusCode)).Inc()
+	}
+}
+
+// improves getting the status code back of the response
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func(lrw *loggingResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code 
+	lrw.ResponseWriter.WriteHeader(code)
 }
 
 func getUsers(w http.ResponseWriter, r *http.Request) {
