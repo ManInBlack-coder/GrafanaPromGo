1bd63981e6b2ff67aad330af8ffd88803e09af6c
Aleksander P <77441538+ManInBlack-coder@users.noreply.github.com>
2025-06-28 12:24:31 +0300

    writed few lines some nonsense that I still don't get but we're getting there

--- server.go
+++ server.go
@@ -1,17 +1,41 @@
 package main
 
 import (
- "fmt"
+ "encoding/json"
  "log"
  "net/http"
+ "github.com/gorilla/mux"
 )
 
-func main() {
- http.HandleFunc("/", handler)
- log.Fatal(http.ListenAndServe(":8080", nil))
+type User struct {
+	ID string `json:"id"`
+	Username string `json:"username"`
+	Email string `json:"email"`
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
- fmt.Fprint(w, "Hello, World!")
+var users []User
+
+func main() {
+ // Initialize a new router
+ router := mux.NewRouter()
+
+ // Define a simple route
+ router.HandleFunc("/users", getUsers).Methods("GET")
+ router.HandleFunc("/users", createUser).Methods("POST")
+
+
+ log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(users)
+
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var newUser User
+	_ = json.NewDecoder(r.Body).Decode(&newUser)
+	users = append(users, newUser)
+	json.NewEncoder(w).Encode(newUser)
 }
 
